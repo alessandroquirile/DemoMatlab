24bae939a300eb4a39069a59daab5ac3528d201f
alessandroquirile <a.quirile@studenti.unina.it>
2022-05-29 15:28:06 +0200

    prova

--- fun.m
+++ fun.m
@@ -1,6 +1,6 @@
 function [outputArg1,outputArg2] = fun(inputArg1,inputArg2)
 %FUN Summary of this function goes here
-%   Detailed explanation goes here
+%   Detailed explanation goes here ok
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
 end
